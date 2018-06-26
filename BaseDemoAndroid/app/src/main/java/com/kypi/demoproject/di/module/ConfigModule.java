d955682bf8b45531e3ff1d38d53b2b5306433ba8
Khoa.Ho <>
2018-06-26 10:26:20 +0700

    [Update] - Config môi trường để in log

--- BaseDemoAndroid/app/src/main/java/com/kypi/demoproject/di/module/ConfigModule.java
+++ BaseDemoAndroid/app/src/main/java/com/kypi/demoproject/di/module/ConfigModule.java
@@ -1,7 +1,9 @@
 package com.kypi.demoproject.di.module;
 
 import com.kypi.demoproject.app.debug.DebugConfigImpl;
+import com.kypi.demoproject.app.debug.DomainLogImpl;
 import com.kypi.demoproject.domain.debugs.DebugConfig;
+import com.kypi.demoproject.domain.debugs.DomainLog;
 
 import javax.inject.Singleton;
 
@@ -15,5 +17,13 @@ public class ConfigModule {
     @Singleton
     public DebugConfig provideDebugConfig() {
         return new DebugConfigImpl();
+    }
+
+
+
+    @Provides
+    @Singleton
+    public DomainLog provideDomainLog() {
+        return new DomainLogImpl();
     }
 }
